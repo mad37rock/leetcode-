4155cf72bc43c6549c60c372b3caa9ee41a52cba
Madhav <114502099+mad37rock@users.noreply.github.com>
2025-09-06 08:51:37 +0530

    Time: 12 ms (97.34%) | Memory: 46.9 MB (35.00%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int res=0;
+        for(int i=0;i<n;i=i+2){
+            res=res+nums[i];
+        }
+        return res;
+    }
+}
